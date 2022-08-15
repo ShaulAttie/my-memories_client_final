60e105211affded80ad40edbe0fc966304e16875
Shaul Attie <solyattie13@gmail.com>
2022-08-15 16:07:30 +0300

    https://mymemories02.herokuapp.com/

--- src/API/index.js
+++ src/API/index.js
@@ -1,8 +1,8 @@
 import axios from "axios"
 
 // const API = axios.create({ baseURL: `http://localhost:5000` })
-const API = axios.create({ baseURL: `https://my-memories13.netlify.app` })
-// https://mymemoriesclientfinal.herokuapp.com/
+const API = axios.create({ baseURL: `https://mymemories02.herokuapp.com/` })
+
 API.interceptors.request.use((req) => {
     if (localStorage.getItem("profile"))
         req.headers.Authorization = `Bearer ${JSON.parse(localStorage.getItem('profile')).token}`
